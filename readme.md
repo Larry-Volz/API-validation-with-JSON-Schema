0e934dbcdced04b028ee70b84848e2cbc1cbf482
Larry Volz <imaginologist@gmail.com>
2021-12-02 02:35:02 -0500

    API validation works for create route

--- readme.md
+++ readme.md
@@ -1,3 +1,19 @@
-Exercise in API validation using JSON Schema
+# Exercise in API validation using JSON Schema 
 
 *Note: to actually utilize this I would put config.js in .gitignore.  I include it here for convenience of testing purposes.  Change that if used for production.
+
+## Example of what book object should look like:
+
+{
+  "isbn": "0691161518",
+  "amazon_url": "http://a.co/eobPtX2",
+  "author": "Matthew Lane",
+  "language": "english",
+  "pages": 264,
+  "publisher": "Princeton University Press",
+  "title": "Power-Up: Unlocking the Hidden Mathematics in Video Games",
+  "year": 2017
+}
+
+## Schema
+schema generated at: https://jsonschema.net/home 
--- routes/books.js
+++ routes/books.js
@@ -2,6 +2,9 @@ const express = require("express");
 const Book = require("../models/book");
 
 const router = new express.Router();
+const ExpressError = require("../expressError")
+const jsonschema = require("jsonschema");
+const bookSchema = require("../schemas/bookSchema.json");
 
 
 /** GET / => {books: [book, ...]}  */
@@ -30,6 +33,14 @@ router.get("/:id", async function (req, res, next) {
 
 router.post("/", async function (req, res, next) {
   try {
+
+    const result = jsonschema.validate(req.body, bookSchema);
+
+    if (!result.valid) {
+      let errList = result.errors.map(error => error.stack);
+      let error = new ExpressError(errList, 400);
+      return next(error);
+    }
     const book = await Book.create(req.body);
     return res.status(201).json({ book });
   } catch (err) {
